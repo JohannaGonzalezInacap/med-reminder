51858c7ef164d76cf6c614f8ac765164dec3cb66
JohannaGonzalezInacap <johanna.gonzalez13@inacapmail.cl>
2026-02-14 07:45:16 -0300

    Use correct Firebase web config

--- config.js
+++ config.js
@@ -6,10 +6,9 @@
       apiKey: "AIzaSyC97Y2EYed-MN_LfAlK_Xe4XBariy6ct1o",
       authDomain: "med-reminder-ab396.firebaseapp.com",
       projectId: "med-reminder-ab396",
-      storageBucket: "med-reminder-ab396.appspot.com",
-      messagingSenderId: "1022485074157",
-      appId: "1:1022485074157:web:8c8c0f8c8c8c8c8c8c8c8c",
-      measurementId: "OPCIONAL_MEASUREMENT_ID"
+      storageBucket: "med-reminder-ab396.firebasestorage.app",
+      messagingSenderId: "916040866686",
+      appId: "1:916040866686:web:c7cde5727063a02772312f"
     },
     vapidKey: "BKFNRtTEhfw0Q5j27w-3Lr7hDU3AG-cWxKv7NZqcrFtguRIMt268yIMsSft4mabJyAGrKET_WRlEntldbE_GM0A"
   };
